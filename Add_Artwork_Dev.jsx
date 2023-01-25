f7912b90215860e45094bf83d5f0132f0605dfd3
williams <31516287+wdjsdev@users.noreply.github.com>
2023-01-25 10:28:00 -0600

    test without getUtilities

--- Add_Artwork_Dev.jsx
+++ Add_Artwork_Dev.jsx
@@ -1,5 +1,5 @@
 #target Illustrator
-function addArtwork()
+function addArtwork ()
 {
 	//global variables
 	var docRef = app.activeDocument,
@@ -23,62 +23,62 @@ function addArtwork()
 		logoType,
 		placedLogos = {};
 
-	var scriptName = "add_artwork"; 
+	var scriptName = "add_artwork";
 
 
-	function getUtilities ()
-	{
-		var utilNames = [ "Utilities_Container" ]; //array of util names
-		var utilFiles = []; //array of util files
-		//check for dev mode
-		var devUtilitiesPreferenceFile = File( "~/Documents/script_preferences/dev_utilities.txt" );
-		function readDevPref ( dp ) { dp.open( "r" ); var contents = dp.read() || ""; dp.close(); return contents; }
-		if ( devUtilitiesPreferenceFile.exists && readDevPref( devUtilitiesPreferenceFile ).match( /true/i ) )
-		{
-			$.writeln( "///////\n////////\nUsing dev utilities\n///////\n////////" );
-			var devUtilPath = "~/Desktop/automation/utilities/";
-			utilFiles =[ devUtilPath + "Utilities_Container.js", devUtilPath + "Batch_Framework.js" ];
-			return utilFiles;
-		}
+	// function getUtilities ()
+	// {
+	// 	var utilNames = [ "Utilities_Container" ]; //array of util names
+	// 	var utilFiles = []; //array of util files
+	// 	//check for dev mode
+	// 	var devUtilitiesPreferenceFile = File( "~/Documents/script_preferences/dev_utilities.txt" );
+	// 	function readDevPref ( dp ) { dp.open( "r" ); var contents = dp.read() || ""; dp.close(); return contents; }
+	// 	if ( devUtilitiesPreferenceFile.exists && readDevPref( devUtilitiesPreferenceFile ).match( /true/i ) )
+	// 	{
+	// 		$.writeln( "///////\n////////\nUsing dev utilities\n///////\n////////" );
+	// 		var devUtilPath = "~/Desktop/automation/utilities/";
+	// 		utilFiles =[ devUtilPath + "Utilities_Container.js", devUtilPath + "Batch_Framework.js" ];
+	// 		return utilFiles;
+	// 	}
 
-		var dataResourcePath = customizationPath + "Library/Scripts/Script_Resources/Data/";
-		
-		for(var u=0;u<utilNames.length;u++)
-		{
-			var utilFile = new File(dataResourcePath + utilNames[u] + ".jsxbin");
-			if(utilFile.exists)
-			{
-				utilFiles.push(utilFile);	
-			}
-			
-		}
+	// 	var dataResourcePath = customizationPath + "Library/Scripts/Script_Resources/Data/";
 
-		if(!utilFiles.length)
-		{
-			alert("Could not find utilities. Please ensure you're connected to the appropriate Customization drive.");
-			return [];
-		}
+	// 	for(var u=0;u<utilNames.length;u++)
+	// 	{
+	// 		var utilFile = new File(dataResourcePath + utilNames[u] + ".jsxbin");
+	// 		if(utilFile.exists)
+	// 		{
+	// 			utilFiles.push(utilFile);	
+	// 		}
 
-		
-		return utilFiles;
+	// 	}
 
-	}
-	var utilities = getUtilities();
+	// 	if(!utilFiles.length)
+	// 	{
+	// 		alert("Could not find utilities. Please ensure you're connected to the appropriate Customization drive.");
+	// 		return [];
+	// 	}
+
+
+	// 	return utilFiles;
+
+	// }
+	// var utilities = getUtilities();
 
 	for ( var u = 0, len = utilities.length; u < len && valid; u++ )
 	{
 		eval( "#include \"" + utilities[ u ] + "\"" );
 	}
 
-	if ( !valid || !utilities.length) return;
+	if ( !valid || !utilities.length ) return;
 
 
-	if(user === "will.dowling")
+	if ( user === "will.dowling" )
 	{
 		DEV_LOGGING = true;
 	}
 
-	logDest.push(getLogDest());
+	logDest.push( getLogDest() );
 
 
 
@@ -87,29 +87,29 @@ function addArtwork()
 	var devPath = "~/Desktop/automation/add_artwork/components";
 	var prodPath = componentsPath + "add_artwork";
 	// var componentFiles = includeComponents(devPath,prodPath,false);
-	var componentFiles = getComponents($.fileName.toLowerCase().indexOf("dev")>-1 ? devPath : prodPath);
+	var componentFiles = getComponents( $.fileName.toLowerCase().indexOf( "dev" ) > -1 ? devPath : prodPath );
 
 	var curPath;
-	if(componentFiles)
+	if ( componentFiles )
 	{
-		for(var f=0;f<componentFiles.length;f++)
+		for ( var f = 0; f < componentFiles.length; f++ )
 		{
-			var thisComponent = componentFiles[f].fullName;
-			log.l(thisComponent);
+			var thisComponent = componentFiles[ f ].fullName;
+			log.l( thisComponent );
 			try
 			{
-				eval("#include \"" + thisComponent + "\"");
+				eval( "#include \"" + thisComponent + "\"" );
 			}
-			catch(e)
+			catch ( e )
 			{
-				log.e("failed to include: " + componentFiles[f].name);
+				log.e( "failed to include: " + componentFiles[ f ].name );
 			}
 		}
 	}
 	else
 	{
 		valid = false;
-		errorList.push("Failed to find the necessary component files.");
+		errorList.push( "Failed to find the necessary component files." );
 	}
 
 	///////Begin/////////
@@ -132,17 +132,17 @@ function addArtwork()
 	////Data Storage////
 	////////////////////
 
-		eval("#include \"" + dataPath + "central_library.js\"");
-		eval("#include \"" + dataPath + "aa_special_instructions.js\"");
+	eval( "#include \"" + dataPath + "central_library.js\"" );
+	eval( "#include \"" + dataPath + "aa_special_instructions.js\"" );
 
-		var library = prepressInfo;
+	var library = prepressInfo;
 
-		if(library == undefined)
-		{
-			log.e("Failed to identify the central library file::Tried to find central_library.js in the following location:::/Volumes/Customization/Library/Scripts/Script Resources/Data/central_library.js")
-			errorList.push("Failed to open the library file. Aborting")
-			valid = false;
-		}
+	if ( library == undefined )
+	{
+		log.e( "Failed to identify the central library file::Tried to find central_library.js in the following location:::/Volumes/Customization/Library/Scripts/Script Resources/Data/central_library.js" )
+		errorList.push( "Failed to open the library file. Aborting" )
+		valid = false;
+	}
 
 	////////End/////////
 	////Data Storage////
@@ -158,70 +158,70 @@ function addArtwork()
 	////////////////////
 
 	//find the mockup layer and paramcolors layer
-	var mockLay = findSpecificLayer(layers[0],"Mockup","any");
-	var paramLay = mockLay ? findSpecificLayer(mockLay,"param","any") : undefined;
+	var mockLay = findSpecificLayer( layers[ 0 ], "Mockup", "any" );
+	var paramLay = mockLay ? findSpecificLayer( mockLay, "param", "any" ) : undefined;
 
-	if(valid)
+	if ( valid )
 	{
-		if(!getGarments(layers))
+		if ( !getGarments( layers ) )
 		{
 			valid = false;
-			log.e("getGarments function failed.");
-		}	
+			log.e( "getGarments function failed." );
+		}
 	}
-	if(valid)
+	if ( valid )
 	{
 		revealPrepressLayersAndItems();
 	}
 
-	if(valid)
+	if ( valid )
 	{
-		if(paramLay)
+		if ( paramLay )
 		{
 			recolorGarment();
 
-			
+
 		}
 		else
 		{
-			errorList.push("Couldn't recolor the document because the Param Colors are missing.");
+			errorList.push( "Couldn't recolor the document because the Param Colors are missing." );
 		}
 	}
 
-	if(valid)
+	if ( valid )
 	{
-		if(!masterLoop(garments))
+		if ( !masterLoop( garments ) )
 		{
 			valid = false;
-			log.e("masterLoop function failed.");
+			log.e( "masterLoop function failed." );
 		}
 	}
 
-	if(valid)
+	if ( valid )
 	{
 		var dupSwatches = [];
 		//check to see if there are duplicate swatches
-		for(var x=0;x<swatches.length;x++)
+		for ( var x = 0; x < swatches.length; x++ )
 		{
-			if(/\sb[\d]$/i.test(swatches[x].name))
-				dupSwatches.push(swatches[x].name);
+			if ( /\sb[\d]$/i.test( swatches[ x ].name ) )
+				dupSwatches.push( swatches[ x ].name );
 		}
-		if(dupSwatches.length)
+		if ( dupSwatches.length )
 		{
-			errorList.push("Don't forget to merge the following duplicate swatches:\n" + dupSwatches.join("\n"));
+			errorList.push( "Don't forget to merge the following duplicate swatches:\n" + dupSwatches.join( "\n" ) );
 		}
 	}
 
-	if(valid)
+	if ( valid )
 	{
 		//if any garment layers were not processed, hide the prepress layer
 		hideSuperfluousPrepressLayers();
 	}
 
 
-	if(errorList.length > 0)
+	if ( errorList.length > 0 )
 	{
-		sendErrors(errorList);
+		sendErrors( errorList );
 	}
 
 	printLog();
