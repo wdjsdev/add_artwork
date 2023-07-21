bd7eba10ae97a8742fa9cce0237f5596af806764
williams <31516287+wdjsdev@users.noreply.github.com>
2023-07-21 14:24:47 -0600

    Update make_clip_mask.js
    error handling for when a backgroundPath cant be determined

--- components/make_clip_mask.js
+++ components/make_clip_mask.js
@@ -3,13 +3,22 @@
 	Author: William Dowling
 	Creation Date: 10 July, 2017
 	Description: 
-		make a clipping mask out of the largest path in the dest piece 
+		make a clipping mask out of the bottom most path in the dest piece
 */
 
 function makeClipMask ( destPiece )
 {
 	var lp = findBackgroundPath( destPiece );
+	if ( !lp )
+	{
+		var bounds = getVisibleBounds( destPiece );
+		lp = destPiece.pathItems.rectangle( bounds[ 1 ], bounds[ 0 ], bounds[ 2 ] - bounds[ 0 ], bounds[ 1 ] - bounds[ 3 ] );
+		lp.name = "geometric_bounds_clip_mask";
+		lp.stroked = lp.filled = false;
+	}
 	clipMask = lp.duplicate( destPiece, ElementPlacement.PLACEATBEGINNING );
+	clipMask.stroked = clipMask.filled = false;
+
 	if ( clipMask.typename.match( /compoundpath/i ) )
 	{
 		clipMask = clipMask.pathItems[ 0 ];
